341701267e47c6d8b40a6c024f0883bea57163ed
Dmitrii Shelonin <cheisesu@gmail.com>
2024-05-13 22:02:06 +0200

    - created project

---
+++ united_communications/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  united_communications
+//
+//  Created by Dmitrii Shelonin on 13/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ united_communications/united_communicationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  united_communicationsApp.swift
+//  united_communications
+//
+//  Created by Dmitrii Shelonin on 13/5/24.
+//
+
+import SwiftUI
+
+@main
+struct united_communicationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
